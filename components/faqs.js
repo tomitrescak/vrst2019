0449e2c84f1aeade6a4632c774eb009a294e2a1f
Tomas Trescak <tomi.trescak@gmail.com>
2019-02-15 09:46:52 +1100

    Updated faqs

--- components/faqs.js
+++ components/faqs.js
@@ -8,7 +8,7 @@ const QnA = [
   },
   {
     Question: 'Who do I ask if my question is not answered here?',
-    Answers: `For more detailed information about accessibility at the VRST 2019, please check the venue accessibility information page, phone the venue information desk at 1-800-268-8122 (toll-free) or check out their <a href="https://www.westernsydney.edu.au/campuses_structure/cas/campuses/parramattacity/one_parramatta_square">web page</a>, or email accessibility.2019@vrst.acm.org. If there is a specific accessibility information you would like to see here, or if you wish to discuss any accessibility requirements, please contact our Accessibility Chairs by email: accessibility.2019@vrst.acm.org, and someone will respond to you shortly.`
+    Answers: `For more detailed information about accessibility at the VRST 2019, please check the venue accessibility information page, phone the venue information desk at +61 2 9683 8188 or check out their <a href="https://www.westernsydney.edu.au/campuses_structure/cas/campuses/parramattacity/one_parramatta_square">web page</a>, or email accessibility.2019@vrst.acm.org. If there is a specific accessibility information you would like to see here, or if you wish to discuss any accessibility requirements, please contact our Accessibility Chairs by email: accessibility.2019@vrst.acm.org, and someone will respond to you shortly.`
   },
   {
     Question: 'When is the deadline for accessibility requests?',
@@ -17,7 +17,7 @@ const QnA = [
   },
   {
     Question: 'Will the conference be accessible by wheelchair?',
-    Answers: `Yes, the conference venue will be accessible by wheelchair. All venue entrances are wheelchair accessible and feature access doors. There is no parking available at the venue, but a paid public parking is available 300 metres from the venue entry on <a href="https://goo.gl/maps/2yvBriMgwhE2">Macquarie stree</a> with slots reserved for individuals with handicap parking permits. A map of the conference space will be made available on the VRST 2019 website shortly. Maps of the venue can be found on the <a href="https://www.westernsydney.edu.au/campuses_structure/cas/campuses/parramattacity/one_parramatta_square">venue web page</a>.`
+    Answers: `Yes, the conference venue will be accessible by wheelchair. All venue entrances are wheelchair accessible and feature access doors. There is no parking available at the venue, but a paid public parking is available 300 metres from the venue entry on <a href="https://goo.gl/maps/2yvBriMgwhE2">Macquarie street</a> with slots reserved for individuals with handicap parking permits. Maps of the venue can be found on the <a href="https://www.westernsydney.edu.au/campuses_structure/cas/campuses/parramattacity/one_parramatta_square">venue web page</a>.`
   },
   {
     Question: 'What are expectations around inclusivity, diversity, and harassment at VRST 2019?',
